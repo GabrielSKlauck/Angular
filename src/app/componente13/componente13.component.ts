b0d56afa9a2c62c4e9fe34a19d1485da0bebc05c
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-07-18 20:34:57 -0300

    Metodo GET e exibição em tabela feito

--- src/app/componente13/componente13.component.ts
+++ src/app/componente13/componente13.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Produto } from '../modelo/Produto';
+import { ProdutoService } from '../servico/produto.service';
 
 @Component({
   selector: 'app-componente13',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './componente13.component.html',
   styleUrl: './componente13.component.css'
 })
 export class Componente13Component {
-  
+  vetor:Produto[] = [];
+
+  btnCadastrar : boolean = true;
+
+  constructor(private servico:ProdutoService){}
+
+  ngOnInit(){
+    this.selecionar();
+  }
+
+  selecionar(){
+    this.servico.selecionar().subscribe(retorno => {
+      this.vetor = retorno;
+    });
+  }
 }
