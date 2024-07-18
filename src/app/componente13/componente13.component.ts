48edf6e8f24834283de97665c37105dd45656177
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-07-18 20:48:46 -0300

    Criação de formulario

--- src/app/componente13/componente13.component.ts
+++ src/app/componente13/componente13.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Produto } from '../modelo/Produto';
 import { ProdutoService } from '../servico/produto.service';
+import { Form, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-componente13',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './componente13.component.html',
   styleUrl: './componente13.component.css'
 })
@@ -14,6 +15,12 @@ export class Componente13Component {
   vetor:Produto[] = [];
 
   btnCadastrar : boolean = true;
+
+  formulario = new FormGroup({
+    id:new FormControl(null),
+    nome:new FormControl(''),
+    valor:new FormControl(null)
+  }); 
 
   constructor(private servico:ProdutoService){}
 
