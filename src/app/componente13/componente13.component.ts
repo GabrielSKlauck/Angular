311203f94ae1316d9072dbb31f24fe45e2bbec39
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-07-18 21:56:47 -0300

    Parte das funções de crud feitas

--- src/app/componente13/componente13.component.ts
+++ src/app/componente13/componente13.component.ts
@@ -33,4 +33,46 @@ export class Componente13Component {
       this.vetor = retorno;
     });
   }
+
+  cadastro(){
+    this.servico.cadastrar(this.formulario.value as Produto)
+    .subscribe(retorno => {
+      this.vetor.push(retorno);
+    });
+    this.formulario.reset();
+  }
+
+  selecionarItem(index:number){
+    this.formulario.setValue({
+      id : this.vetor[index].id,
+      nome : this.vetor[index].nome,
+      valor : this.vetor[index].valor
+    });
+    this.btnCadastrar = false;
+  }
+
+  alterar(){
+    this.servico.alterar(this.formulario.value as Produto)
+    .subscribe(retorno =>{
+
+      let indiceAlterado = this.vetor.findIndex(obj =>{
+        return this.formulario.value.id ===obj.id;
+      });
+
+      this.vetor[indiceAlterado] = retorno;
+
+      this.formulario.reset();
+
+      this.btnCadastrar = true;
+    });
+  }
+
+  excluir(){
+
+  }
+
+  cancelar(){
+    this.formulario.reset();
+    this.btnCadastrar = true;
+  }
 }
