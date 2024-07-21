342dacc0267253400868baac908a5d06838f5a93
Gabriel de Souza Klauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-07-21 19:42:55 -0300

    Input e Output

--- src/app/componente15-formulario/componente15-formulario.component.ts
+++ src/app/componente15-formulario/componente15-formulario.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-componente15-formulario',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './componente15-formulario.component.html',
   styleUrl: './componente15-formulario.component.css'
 })
 export class Componente15FormularioComponent {
+  nome:string ='';
 
+  @Output() funcao = new EventEmitter<string>();
+
+  cadastrarNome(){
+    this.funcao.emit(this.nome);
+  }
+
+  
 }
